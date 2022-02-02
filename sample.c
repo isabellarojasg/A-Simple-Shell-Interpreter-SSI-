cb6fdca32aa2364f85572a7df08988cb80f97d96
Isabella <isabellarojas@uvic.ca>
2022-02-02 11:18:13 -0800

    added malloc to fix segmentation fault

--- sample.c
+++ sample.c
@@ -6,19 +6,38 @@
 #include <unistd.h>
 #include <limits.h>
 char * host_name();
+//char* prompt;
 
 int main(){
 
 	char * prompt= host_name();
 
+
 	int bailout = 0;
 	while (!bailout) {
 
-		char* reply = readline(prompt);
-		/* Note that readline strips away the final \n */
-		/* For Perl junkies, readline automatically chomps the line read */
+	/*
+		char line[1024];
+		char* args[1024];
+		fgets(line,1024,stdin);
+		
+		args[0]=strtok(line," \n");//split string by space or '\n' (IMPORTANT) there will be error without '\n'
+    	
+		printf("Output 1: %s, %s \n",args[0],args[1]);
+    	
+		int i=0;
+    	while(args[i]!=NULL){//make sure that args has a NULL pointer at the end
+       		args[i+1]=strtok(NULL," \n");
+        	i++;
+        }
+    	for(int j=0; j<i; j++){
+        	printf("Output 2: %s \n",args[j]);
+		}
+		*/
+		
 
-		//exit\n command
+		char* reply = readline(prompt);
+		
 		if (!strcmp(reply, "bye")) {
 			bailout = 1;
 		} else {
@@ -26,8 +45,10 @@ int main(){
 		}
 	
 		free(reply);
+		
 	}
 	printf("Bye Bye\n");
+	free(prompt);
 }
 
 char * host_name(){
@@ -47,16 +68,16 @@ char * host_name(){
 
 	int size_of_user = strlen(user_name);
 
-	char *t;
-	char *host= host_name;
+	char* t;
+	char* host = host_name;
 	char* x;
-	char* prompt;
-	for (t =  user_name, x = prompt; *t != '\0'; t++, x++){
+	char* prompt = (char *)malloc(sizeof(char));
+	for (t = user_name, x = prompt; *t != '\0'; t++, x++){
 		*x = *t;
 	}
 	*x= '@';
 	x++;
-	for (t =  host; *t != '\0'; t++, x++){
+	for (t = host; *t != '\0'; t++, x++){
 		*x = *t;
 	}
 	*x= '>'; x++;
